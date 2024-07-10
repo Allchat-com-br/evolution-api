677895bb23b37af6d8df2ab95574f4cbb10fef83
Vinicius M. Almeida <vinicius.sec21@gmail.com>
2024-07-10 12:53:09 -0300

    Ajuste docs e rota inicial

--- src/docs/swagger.conf.ts
+++ src/docs/swagger.conf.ts
@@ -11,7 +11,7 @@ export const swaggerRouter = router.use('/docs', swaggerUi.serve).get(
   '/docs',
   swaggerUi.setup(document, {
     customCssUrl: '/css/dark-theme-swagger.css',
-    customSiteTitle: 'Evolution API',
-    customfavIcon: '/images/logo.svg',
+    customSiteTitle: 'Allchat Manager API',
+    customfavIcon: 'https://allchat.com.br/wp-content/uploads/2023/11/cropped-logo-allchat-sua-plataforma-de-atendimento-ao-cliente-32x32.png',
   }),
 );
